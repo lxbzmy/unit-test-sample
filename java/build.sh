c2c5d5c7620fea098d2cd49e3e560be49cc74068
leixiaobao <lxbzmy@gmail.com>
2026-03-28 14:03:57 +0800

    升级mvn3.9，添加覆盖率report

--- java/build.sh
+++ java/build.sh
@@ -1,8 +1,12 @@
-#!/bin/env bash
-export MAVEN_USER_HOME=$(pwd)/m2
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export MAVEN_USER_HOME="$(pwd)/m2"
+
 for dir in *; do
     if [ -d "$dir" ] && [ -f "$dir/pom.xml" ]; then
-    echo "Building and testing $dir"
-    ./mvnw -f "$dir/pom.xml" clean package
+        echo "run mvn site for $dir"
+        ./mvnw -f "$dir/pom.xml" clean site
     fi
 done
